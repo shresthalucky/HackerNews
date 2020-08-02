116a933303e8b4afc2339611443b7a5489303368
Lucky Shrestha <luckshrestha@gmail.com>
2020-08-02 21:07:31 +0545

    Updates

---
+++ precache-manifest.7ac052f4ad566240473c6addbb223a9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47418ec49ee231f8794698f8a0b05fa8",
+    "url": "/HackerNews/index.html"
+  },
+  {
+    "revision": "90647a944c4be5b15aec",
+    "url": "/HackerNews/static/css/main.948e9768.chunk.css"
+  },
+  {
+    "revision": "272c8c71e58821c06faf",
+    "url": "/HackerNews/static/js/2.e9e38c02.chunk.js"
+  },
+  {
+    "revision": "0b08544f54c9906705be39c03b7e79ad",
+    "url": "/HackerNews/static/js/2.e9e38c02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90647a944c4be5b15aec",
+    "url": "/HackerNews/static/js/main.a966927d.chunk.js"
+  },
+  {
+    "revision": "7a94f183fd50e58d85d3",
+    "url": "/HackerNews/static/js/runtime-main.6f672ce1.js"
+  }
+]);
